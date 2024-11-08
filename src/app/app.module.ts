d7510ebf0a1c6bb8fce6d58f258c47b361ecf1e3
matiasgarridopinto <139499088+matiasgarridopinto@users.noreply.github.com>
2024-11-08 19:34:28 -0300

    [200~Se creo el funcionamiento de calendario ion-datetime y se agrega en el app.module.ts para el funcionamiento en el inicio de la aplicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -6,7 +6,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms'; // Importa FormsModule
 
 // Importaciones de Firebase y AngularFire
 import { AngularFireModule } from '@angular/fire/compat';
@@ -19,13 +19,20 @@ import { AuthService } from './services/auth.service';
 // Importa HttpClientModule
 import { HttpClientModule } from '@angular/common/http';
 
+// Importa el componente IonDatetimeModalComponent
+import { IonDatetimeModalComponent } from './ion-datetime-modal/ion-datetime-modal.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    IonDatetimeModalComponent  // Agrega el componente modal aquí
+  ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
     ReactiveFormsModule,
+    FormsModule,  // Añadir FormsModule para ngModel
     HttpClientModule, // Añadir HttpClientModule aquí
     // Inicialización de Firebase
     AngularFireModule.initializeApp(environment.firebaseConfig),
@@ -36,5 +43,6 @@ import { HttpClientModule } from '@angular/common/http';
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]  // Agregar CUSTOM_ELEMENTS_SCHEMA aquí
 })
 export class AppModule {}
