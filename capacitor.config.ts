68a1098648354a7ebe8316261ebd9532ea2285a1
RafaOteiza <133901503+RafaOteiza@users.noreply.github.com>
2024-11-18 11:34:41 -0300

    Actualizacion
    Actualizacion completa

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,17 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.duoc.assist.v2',
+  appId: 'com.example.duocassistv2',
   appName: 'DuocAssistV2',
-  webDir: 'www'
+  webDir: 'www',
+  bundledWebRuntime: false,
+  plugins: {
+    LocalNotifications: {
+      smallIcon: 'ic_notification', // Icono pequeño
+      iconColor: '#488AFF',         // Color del icono
+      sound: 'beep.wav',            // Sonido opcional
+    },
+  },
 };
 
 export default config;
