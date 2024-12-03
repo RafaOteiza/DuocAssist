fcea37ec9f86e7f9bf6ae7fad15bbc762eaaa517
RafaOteiza <133901503+RafaOteiza@users.noreply.github.com>
2024-12-03 14:43:42 -0300

    Add files via upload
    Aplicación Terminada

--- src/app/models/asignatura.model.ts
+++ src/app/models/asignatura.model.ts
@@ -1,9 +1,10 @@
-// src/app/models/asignatura.model.ts
 export interface Asignatura {
     dia: string;           // Día de la asignatura (por ejemplo, "Lunes")
     horario: string;       // Horario de la clase (por ejemplo, "19:00 - 20:20")
     nombre: string;        // Nombre de la asignatura (por ejemplo, "Ética para el Trabajo")
-    seccion: string;       // Sección de la asignatura (por ejemplo, "EAY4470")
-    sala: string;          // Sala donde se imparte la asignatura (por ejemplo, "SJ-706")
-    profesorEmail?: string; // Añadir campo opcional profesor
-}
+    sigla: string;         // Sigla de la asignatura (por ejemplo, "PGY4121")
+    seccion: string;       // Sección de la asignatura (por ejemplo, "008V")
+    sala: string;          // Sala donde se imparte la asignatura (por ejemplo, "SJ-L706")
+    profesorEmail?: string; // Campo opcional para el correo del profesor
+  }
+  
