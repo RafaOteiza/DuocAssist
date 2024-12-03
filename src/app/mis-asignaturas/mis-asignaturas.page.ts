db425baee8f02d361ec8ae0be480dcb6711e8800
RafaOteiza <133901503+RafaOteiza@users.noreply.github.com>
2024-12-03 14:43:15 -0300

    Add files via upload
    Aplicación Terminada

--- src/app/mis-asignaturas/mis-asignaturas.page.ts
+++ src/app/mis-asignaturas/mis-asignaturas.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AsignaturaService } from '../services/asignatura.service';
 import { Asignatura } from '../models/asignatura.model';
 
+
 @Component({
   selector: 'app-mis-asignaturas',
   templateUrl: './mis-asignaturas.page.html',
@@ -10,6 +11,8 @@ import { Asignatura } from '../models/asignatura.model';
 export class MisAsignaturasPage implements OnInit {
   asignaturasPorDia: { [key: string]: Asignatura[] } = {};
   diasOrdenados: string[] = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+  anioActual: number = new Date().getFullYear(); // Año académico actual
+  mesActual: number = new Date().getMonth() + 1; // Mes actual
 
   constructor(private asignaturaService: AsignaturaService) {}
 
@@ -19,7 +22,11 @@ export class MisAsignaturasPage implements OnInit {
 
   organizarAsignaturasPorDia() {
     this.diasOrdenados.forEach(dia => {
-      this.asignaturasPorDia[dia] = this.asignaturaService.getAsignaturasPorDia(dia);
+      this.asignaturasPorDia[dia] = this.asignaturaService.getAsignaturasPorDiaYAnio(
+        dia,
+        this.anioActual,
+        this.mesActual
+      );
     });
   }
 }
